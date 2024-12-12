372b93b42011707d6aae4389d87dd8060a88fe4a
emad eissa <emad.eissa999333@gmail.com>
2024-12-13 00:59:51 +0200

    Update GlobalUsing.cs
    ererer

--- ProApiFull.Domin/GlobalUsing.cs
+++ ProApiFull.Domin/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using ProApiFull.Domin.Entities;
 global using System.ComponentModel.DataAnnotations.Schema;
+//////////////////////////////
